ba7d471138f75641ff626c526b8e7012b172ba49
josedubrez <jldubrez@gmail.com>
2021-10-28 17:15:55 -0300

    Tercer commit

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,9 +1,9 @@
 window.addEventListener('dfMessengerLoaded',function(event) {
     const df_messenger = document.querySelector('df-messenger');
-    //df_messenger.renderCustomText('Buenas noches');
+    df_messenger.renderCustomText('Buenas noches');
     df_messenger.addEventListener('df-list-element-clicked', function(event){
         alert('Has pulsado la opcion '+event.detail.element.title)
-        fetch('http://localhost:4000/tasks/')
+        fetch('http://localhost:3000/servicios/')
             .then(response => response.json())
             .then(data => console.log(data))
             .then(error => console.log(error))
