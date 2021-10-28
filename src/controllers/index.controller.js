2bc99db9f5652e7b4c81647cd1ffd4ecbd543a3a
josedubrez <jldubrez@gmail.com>
2021-10-28 18:40:16 -0300

    Se agrega credenciales del servicio de Postgres de Heroku

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const config = {
-    user: 'postgres',
-    host: 'localhost',
-    password:'postgres',
-    database: 'postgres'
+    user: 'wdubdcqfwuzars',
+    host: 'ec2-54-159-176-167.compute-1.amazonaws.com',
+    password:'77a9ed59b8e487d0b7068772d1cee02fcab03d0afef2f2f0a2874959e6593d8b',
+    database: 'd2545sl3d2sgsr'
 }
 
 const pool = new Pool(config)
