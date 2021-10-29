f05ff08d89d0477596cb01fa276909ca79635c44
josedubrez <jldubrez@gmail.com>
2021-10-28 21:29:28 -0300

    Correccion configuracion heroku

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const config = {
     user: 'wdubdcqfwuzars',
-    host: 'ec2-54-159-176-167.compute-1.amazonaws.com',
+    host: 'https://ec2-54-159-176-167.compute-1.amazonaws.com',
     password:'77a9ed59b8e487d0b7068772d1cee02fcab03d0afef2f2f0a2874959e6593d8b',
     database: 'd2545sl3d2sgsr'
 }
