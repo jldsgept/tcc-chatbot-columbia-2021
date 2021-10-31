7c64d441274e997de25058ed08592f885037e380
josedubrez <jldubrez@gmail.com>
2021-10-31 15:33:06 -0300

    Correcion en el pool de conexion

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const config = {
-    host: 'https://ec2-54-159-176-167.compute-1.amazonaws.com',
+    host: 'ec2-54-159-176-167.compute-1.amazonaws.com',
     port: "5432",
     user: 'wdubdcqfwuzars',
     password:'77a9ed59b8e487d0b7068772d1cee02fcab03d0afef2f2f0a2874959e6593d8b',
