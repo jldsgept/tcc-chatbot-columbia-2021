3b0e4054a7b102362835d9f76323cc376fd1a29b
josedubrez <jldubrez@gmail.com>
2021-10-31 18:56:14 -0300

    Correccion control de Cors

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ const port = process.env.PORT || 3000
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    next();
+});
 
 // Routes
 app.use(require('./routes/index'));
