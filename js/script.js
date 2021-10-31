e1639c70979cd942a2052eacd29fbcfcb52c0778
josedubrez <jldubrez@gmail.com>
2021-10-31 19:40:32 -0300

    Updates

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ const url_base = 'https://tcc-chatbot-columbia-2021.herokuapp.com/'
 
 window.addEventListener('dfMessengerLoaded',function(event) {
     const df_messenger = document.querySelector('df-messenger');
-    df_messenger.renderCustomText('Buenas noches');
+    //df_messenger.renderCustomText('Buenas noches');
     df_messenger.addEventListener('df-list-element-clicked', function(event){
-        alert('Has pulsado la opcion '+event.detail.element.title)
+        //alert('Has pulsado la opcion '+event.detail.element.title)
         fetch(url_base + 'servicios')
             .then(response => response.json())
             .then(data => console.log(data))
