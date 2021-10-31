b5c7015dae62b4003ea447c4b124e0eece427b7e
josedubrez <jldubrez@gmail.com>
2021-10-31 13:26:03 -0300

    Cambios en el pool

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,10 +1,14 @@
 const { Pool } = require('pg');
 
 const config = {
-    user: 'wdubdcqfwuzars',
     host: 'https://ec2-54-159-176-167.compute-1.amazonaws.com',
+    port: "5432",
+    user: 'wdubdcqfwuzars',
     password:'77a9ed59b8e487d0b7068772d1cee02fcab03d0afef2f2f0a2874959e6593d8b',
-    database: 'd2545sl3d2sgsr'
+    database: 'd2545sl3d2sgsr',
+    ssl: {
+        rejectUnauthorized: false
+    }
 }
 
 const pool = new Pool(config)
