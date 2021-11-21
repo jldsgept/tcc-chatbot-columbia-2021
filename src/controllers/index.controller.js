baedefb617c3abe2e2414cc5021618f1ecaa902e
josedubrez <jldubrez@gmail.com>
2021-11-21 19:47:25 -0300

    Correccion prueba  20

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -48,14 +48,15 @@ const webhook = async (req, res) => {
         try{
             rs = await pool.query(sqlstring)
             agent.add('Estos son los servicios que tenemos de '+ tipo_servicio);
-            agent.add(`${rs.rowCount}`);
             for (let i = 0; i <= (rs.rowCount - 1); i++) {
-                agent.add(`Imprimiendo desde el for: ${i}`);
-                card = new Card({   title: rs.rows[i].p_servicio,
-                                    imageUrl: rs.rows[i].p_url_imagen,
-                                    text: rs.rows[i].p_especificaciones
-                                })
-                agent.add(JSON.stringify(card));
+                agent.add(new Card({
+					                title: `Title: this is a card title`,
+                                    imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
+                                    text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
+                                    buttonText: 'This is a button',
+                                    buttonUrl: 'https://assistant.google.com/'
+				                    })
+			            )
             }
             agent.add('Le interesa algun otro servicio?');
         }catch(e){
