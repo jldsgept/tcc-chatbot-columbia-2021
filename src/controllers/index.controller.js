8299599759ca568532fac34e47066380e2246da4
josedubrez <jldubrez@gmail.com>
2021-11-23 05:37:53 -0300

    Se agrega funcion para intent VerFactura (2)

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -46,7 +46,7 @@ const webhook = async (req, res) => {
         try{
             rs = await pool.query(sqlstring)
             for (let i = 0; i <= (rs.rowCount - 1); i++) {
-                agent.add(`Hola ${rs.rows[i].p_nombre} su proxima factura por el monto de ${rs.rows[i].p_monto} GS vence en fecha ${rs.rows[i].p_vencimiento}`)
+                agent.add(`Hola ${rs.rows[i].p_nombre}, su proxima factura es por el monto de ${rs.rows[i].p_monto} GS y vence en fecha ${rs.rows[i].p_vencimiento}`)
             }
             agent.add('Le podemos ayudar en algo mas?');
         }catch(e){
