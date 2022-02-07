bac99959994a546dd9428a819b0ce96aaf1976d8
josedubrez <jldubrez@gmail.com>
2022-02-07 17:13:02 -0300

    Updates

--- js/script.js
+++ js/script.js
@@ -1,29 +1,11 @@
-/*const url_base = 'https://tcc-chatbot-columbia-2021.herokuapp.com/'
+const FORM = document.querySelector('#form')
+const SEND_MAIL = document.querySelector('#send_mail')
 
-window.addEventListener('dfMessengerLoaded',function(event) {
-    const df_messenger = document.querySelector('df-messenger');
-    //df_messenger.renderCustomText('Buenas noches');
-    df_messenger.addEventListener('df-list-element-clicked', function(event){
-        //alert('Has pulsado la opcion '+event.detail.element.title)
+FORM.addEventListener('submit', handleSubmit)
 
-        if (event.detail.element.title === "Soy Cliente") {
-            fetch(url_base + 'facturas')
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data)
-                    df_messenger.renderCustomText("Respuesta de la base de datos");
-                    df_messenger.renderCustomText("Vencimiento: " + data[0].vencimiento + " (Monto : "+ data[0].monto + " GS al mes)");
-                })
-                .then(error => console.log(error))
-        }else {
-            fetch(url_base + 'servicios')
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data)
-                    df_messenger.renderCustomText("Respuesta de la base de datos");
-                    df_messenger.renderCustomText(data[0].p_servicio + " (Precio : "+ data[0].p_precio + " GS al mes)");
-                })
-                .then(error => console.log(error))
-        }
-    })
-});*/
+function handleSubmit(event){
+    event.preventDefault()
+    const a_form = new FormData(this)
+    SEND_MAIL.setAttribute('href',`mailto:jldubrez@gmail.com?subject=Mensaje de ${a_form.get('nombre')} (${a_form.get('email')})&body=${a_form.get('contenido')}`)
+    SEND_MAIL.click()
+}
