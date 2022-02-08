1914618718b6841d5458c268928cc630857b2ac7
josedubrez <jldubrez@gmail.com>
2022-02-08 08:39:58 -0300

    Funcion correo (1)

--- docs/js/script.js
+++ docs/js/script.js
@@ -6,6 +6,6 @@ FORM.addEventListener('submit', handleSubmit)
 function handleSubmit(event){
     event.preventDefault()
     const a_form = new FormData(this)
-    SEND_MAIL.setAttribute('href',`mailto:jldubrez@gmail.com?subject=Mensaje de ${a_form.get('nombre')} (${a_form.get('email')})&body=${a_form.get('contenido')}`)
+    SEND_MAIL.setAttribute('href',`mailto:jldubrez@gmail.com?subject=prueba&body=${a_form.get('contenido')}`)
     SEND_MAIL.click()
 }
