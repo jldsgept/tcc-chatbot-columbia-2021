f5f310c7881ed006d661d6e203ca9173a01b730a
josedubrez <jldubrez@gmail.com>
2022-02-10 10:35:26 -0300

    Se agrega manejador de intencion VerTicket 8

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -129,8 +129,8 @@ const webhook = async (req, res) => {
         sqlstring = `SELECT * FROM f_get_estado_ticket(${nro_ticket})`
         agent.add(sqlstring)
         try{
-            rs = await pool.query(sqlstring)
-            /*for (let i = 0; i <= (rs.rowCount - 1); i++) {
+            /*rs = await pool.query(sqlstring)
+            for (let i = 0; i <= (rs.rowCount - 1); i++) {
                 agent.add(`Estimado cliente ${rs.rows[i].p_cliente} su ticket numero ${nro_ticket} de prioridad ${rs.rows[i].p_cliente} se encuentra en estado ${rs.rows[i].p_estado} y esta siendo atendido por el departamento de ${rs.rows[i].p_departamento}`);
             }
             if (rs.rowCount === 0) {
