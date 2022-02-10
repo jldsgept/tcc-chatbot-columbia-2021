045199933cfb6984d69f595dcb1210d0ea4dadc0
josedubrez <jldubrez@gmail.com>
2022-02-10 11:50:13 -0300

    Se agrega manejador de intencion VerTicket 17

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -37,17 +37,14 @@ const webhook = async (req, res) => {
     const agent = new WebhookClient({ request: req, response: res });
     let intentMap = new Map();
 
-    function ticket(agent) {
+    async function ticket(agent) {
         let sqlstring, nro_ticket, rs
         nro_ticket = agent.parameters['number']
-        //sqlstring = `SELECT * FROM f_get_estado_ticket(${nro_ticket})`
-        sqlstring = `SELECT 1 as data`
-        agent.add(sqlstring)
+        sqlstring = `SELECT * FROM f_get_estado_ticket(${nro_ticket})`
         try{
             rs = await pool.query(sqlstring)
             for (let i = 0; i <= (rs.rowCount - 1); i++) {
-                //agent.add(`Estimado cliente ${rs.rows[i].p_cliente} su ticket numero ${nro_ticket} de prioridad ${rs.rows[i].p_cliente} se encuentra en estado ${rs.rows[i].p_estado} y esta siendo atendido por el departamento de ${rs.rows[i].p_departamento}`);
-                agent.add(`Se recupero datos`);
+                agent.add(`Estimado cliente ${rs.rows[i].p_cliente} su ticket numero ${nro_ticket} de prioridad ${rs.rows[i].p_cliente} se encuentra en estado ${rs.rows[i].p_estado} y esta siendo atendido por el departamento de ${rs.rows[i].p_departamento}`);
             }
             if (rs.rowCount === 0) {
                 agent.add(`No hemos encontrado informacion sobre el ticket ${nro_ticket} que consulto`);
