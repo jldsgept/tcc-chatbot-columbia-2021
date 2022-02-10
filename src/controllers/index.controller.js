c65dbf6c7871b3c6fd88261f69e5fefab89fac63
josedubrez <jldubrez@gmail.com>
2022-02-10 11:41:35 -0300

    Se agrega manejador de intencion VerTicket 15

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -42,18 +42,18 @@ const webhook = async (req, res) => {
         nro_ticket = agent.parameters['number']
         sqlstring = `SELECT * FROM f_get_estado_ticket(${nro_ticket})`
         agent.add(sqlstring)
-        /*try{
-            rs = await pool.query(sqlstring)
+        try{
+            /*rs = await pool.query(sqlstring)
             for (let i = 0; i <= (rs.rowCount - 1); i++) {
                 agent.add(`Estimado cliente ${rs.rows[i].p_cliente} su ticket numero ${nro_ticket} de prioridad ${rs.rows[i].p_cliente} se encuentra en estado ${rs.rows[i].p_estado} y esta siendo atendido por el departamento de ${rs.rows[i].p_departamento}`);
             }
             if (rs.rowCount === 0) {
                 agent.add(`No hemos encontrado informacion sobre el ticket ${nro_ticket} que consulto`);
-            }
+            }*/
             agent.add('Le podemos ayudar en algo mas?');
         }catch(e){
             agent.add(e)
-        }*/
+        }
     }
 
     async function crearTicket(agent) {
