5f2796083c9f59d5bc04e6665ab95b05755aee1a
josedubrez <jldubrez@gmail.com>
2022-02-10 10:06:44 -0300

    Se agrega manejador de intencion VerTicket

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -122,7 +122,21 @@ const webhook = async (req, res) => {
     }
 
     function verTicket(agent) {
-        agent.add(`Ver Ticket`);
+        let sqlstring, nro_ticket, rs
+        nro_ticket = agent.parameters['number']
+        sqlstring = `SELECT * FROM f_get_estado_ticket('${nro_ticket}')`
+        try{
+            rs = await pool.query(sqlstring)
+            for (let i = 0; i <= (rs.rowCount - 1); i++) {
+                agent.add(`Estimado cliente ${rs.rows[i].p_cliente} su ticket numero ${nro_ticket} se encuentra en estado ${rs.rows[i].p_estado} y esta siendo atendido por el departamento de ${rs.rows[i].p_departamento}`);
+            }
+            if (rs.rowCount === 0) {
+                agent.add(`No hemos encontrado informacion sobre el ticket ${nro_ticket} que consulto`);
+            }
+            agent.add('Le podemos ayudar en algo mas?');
+        }catch(e){
+            agent.add(e)
+        }
     }
 
     intentMap.set('VerTicket', verTicket);
