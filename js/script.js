8db29947da6406c1655fabeda443590bf56374a0
josedubrez <jldubrez@gmail.com>
2022-03-04 11:21:16 -0300

    Updates

--- js/script.js
+++ js/script.js
@@ -4,12 +4,12 @@ FORM.addEventListener('submit', handleSubmit)
 function handleSubmit(event){
     event.preventDefault()
     swal('Su mensaje ha sido enviado!!','Muchas Gracias','success');
-    /*const FORM_DATA = new FormData(this)
-    let name, email, subject, message
-    name = FORM_DATA.get('nombre')
-    email = FORM_DATA.get('email')
-    subject = `Mensaje de ${name} (${email})`
-    message = FORM_DATA.get('contenido')
-    SEND_MAIL.setAttribute('href',`mailto:jldsgept.tcc2021@gmail.com?subject=${subject}&body=${message}`)
-    SEND_MAIL.click()*/
+
+    let nombre, email, contenido
+    nombre = document.getElementById('nombre');
+    email = document.getElementById('email');
+    contenido = document.getElementById('contenido');
+    nombre.value = '';
+    email.value = '';
+    contenido.value = '';
 }
