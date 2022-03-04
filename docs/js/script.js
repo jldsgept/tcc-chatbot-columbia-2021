721134c187edcbe07bd907fffae44531eb85e391
josedubrez <jldubrez@gmail.com>
2022-03-04 11:43:10 -0300

    mensajes de correo (9

--- docs/js/script.js
+++ docs/js/script.js
@@ -2,6 +2,6 @@ const FORM = document.querySelector('#form')
 FORM.addEventListener('submit', handleSubmit)
 
 function handleSubmit(event){
-    swal('Su mensaje ha sido enviado!!!','Muchas Gracias','success');
+    swal('Mensaje Enviado!!!','Muchas Gracias','success');
     FORM.reset();
 }
